7f75e3c94c87a8bb0a2af2922c0c20910d6c7ca1
jams1811 <39497858+jams1811@users.noreply.github.com>
2020-06-29 19:11:09 +0100

    Delete projects on home page

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
 class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=250)
     image = models.ImageField(upload_to='portfolio/images')
     url = models.URLField(blank=True)
-
 
 #This function is to make the name of the object whatever you want like the title
 #It needs to be indented
--- portfolio/views.py
+++ portfolio/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Project
 
+
 # Create your views here.
 
 def home(request):
